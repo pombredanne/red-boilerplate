8c0dc3a9bc49a267efd4967aa0671fdc0e08d536
Richard Herrera <rich@doctyper.com>
2012-08-22 12:39:52 -0700

    Updating paths to PhantomJS runner. Passing extra parameters.

--- resources/tasks/oxblood.js
+++ resources/tasks/oxblood.js
@@ -2,22 +2,27 @@
 module.exports = function (grunt) {
 
 	grunt.registerTask("oxblood", "Run Rosy + Mocha unit tests", function (mode) {
+		var done = this.async();
+
+		var fs = require("fs");
 		var cp = require("child_process");
-		var runner = "test/runner.js";
+
+		var jsDir = "project/static/js/";
+		var runner = jsDir + "test/runner.js";
 
 		if (!fs.existsSync(runner)) {
 			console.error("OxBlood not found.");
 			process.exit();
 		}
 
-		var child = cp.spawn("node", [runner, "-m", mode], {
+		var child = cp.spawn("node", [runner, "-m", mode, "-r", jsDir], {
 			env: null,
 			setsid: true,
 			stdio: "inherit"
 		});
 
 		child.addListener("exit", function () {
-			process.exit();
+			done();
 		});
 	});
 
